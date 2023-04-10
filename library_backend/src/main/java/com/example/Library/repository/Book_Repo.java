76ed80da3d5c3c019569ae3ccd215ed1d796c9e2
Jessjes <113330401+Jessjes@users.noreply.github.com>
2023-04-10 13:15:24 +0800

    Delete library_backend directory

--- library_backend/src/main/java/com/example/Library/repository/Book_Repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Library.repository;
-
-import com.example.Library.model.Books;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface Book_Repo extends JpaRepository<Books, Integer> {
-}
